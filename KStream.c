f5fd3db2062049e06df8cad53731d1e8ad24ee52
Ming E Creekmore <mec5765@cs.rit.edu>
2022-03-31 17:10:49 -0400

    Created file buffers in mop_message.c and created empty template in KStream.c

--- KStream.c
+++ KStream.c
@@ -8,4 +8,22 @@
 //
 // // // // // // // // // // // // // // // // // // // // //
 
+typedef struct stream {
+    unsigned char *key;
+    unsigned long keylen;
+    unsigned char s[256];
+    unsigned int i;
+    unsigned int j;
+} *kstreamADT;
 
+kstreamADT ks_create(FILE *input) {
+
+}
+
+kstreamADT ks_destroy(kstreamADT) {
+
+}
+
+kstreamADT ks_translate(kstreamADT) {
+
+}
--- mop_message.c
+++ mop_message.c
@@ -9,3 +9,26 @@
 //
 // // // // // // // // // // // // // // // // // // // // // // // //
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <KStreamADT.h>
+
+int main(int argc, char*argv[]) {
+    FILE *keyfile;
+    FILE *infile;
+    FILE *outfile;
+    if(argc != 4) {
+        fprintf(stderr, "Usage: mop_message keyfile in-file [ out-file | - ]\n");
+        return EXIT_FAILURE;
+    }
+
+    keyfile = fopen(argv[1], "rb");
+    infile = fopen(argv[2], "r");
+    if(argv[3] == "-") {
+        outfile = stdout;
+    }
+    else {
+        outfile = fopen(argv[3], "wb");
+    }
+
+}
